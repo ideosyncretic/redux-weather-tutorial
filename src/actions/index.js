8f5a67241a25d6ba7d07cc465f217f52c22e249a
Angel Chen <angel@finaxar.com>
2017-06-09 14:17:03 +0800

    Added axios get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const API_KEY = 'fd5019452695be9557a5e67f2c2fa6b2'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
@@ -5,7 +7,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather (city) {
   const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url) // this returns a promise
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
