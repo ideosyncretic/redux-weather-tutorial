94f8c8fc1d02cd01ae459037cf0b0155ab600a1e
Angel Chen <angel@finaxar.com>
2017-06-09 14:14:15 +0800

    Added root url and fetchWeather query params

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 const API_KEY = 'fd5019452695be9557a5e67f2c2fa6b2'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather () {
+export function fetchWeather (city) {
+  const url = `${ROOT_URL}&q=${city},us`
   return {
     type: FETCH_WEATHER
   }
